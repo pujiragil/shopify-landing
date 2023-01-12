f4ab1b2ffa920677c9b3d95c3913ac49a1b782c0
pujiragil <ragilsg164@gmail.com>
2023-01-12 21:43:54 +0700

    config: add some customize colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,10 +7,21 @@ module.exports = {
   theme: {
     colors: {
       primary: {
-        100: "#004C3F"
+        100: "#004C3F",
+        200: "#9DEDC6"
       },
       black: {
-        100: "#212625"
+        100: "#212625",
+        200: "#424C4A",
+        300: "#57605E",
+        400: "#82918E"
+      },
+      secondary: {
+        200: "#FFBC9F"
+      },
+      tertiary: {
+        200: "#FFDB95",
+        300: "#FBF7EC"
       }
     },
     extend: {},
