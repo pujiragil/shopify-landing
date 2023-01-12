8ff794b0989f52bc86de2b21ea6393cf837fa876
pujiragil <ragilsg164@gmail.com>
2023-01-12 22:59:23 +0700

    nav: create nav menu

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -12,10 +12,19 @@ export default function Navbar() {
         <img className="w-[125px]" src="/shopify-header.png" alt="shopify-icon" />
 
         {/* Hamburger */}
-        <div onClick={handleOpen} className={`${isOpen ? "open" : ""} space-y-[7px] cursor-pointer md:hidden`}>
-          <span className="block transition-all duration-500 ease-in-out h-[2px] w-5 bg-black-500 rounded-full"></span>
-          <span className="block transition-all duration-500 ease-in-out h-[2px] w-4 mx-auto bg-black-500 rounded-full"></span>
-          <span className="block transition-all duration-500 ease-in-out h-[2px] w-5 bg-black-500 rounded-full"></span>
+        <div onClick={handleOpen} className={`${isOpen ? "open" : ""} flex flex-col items-center w-fit gap-[7px] cursor-pointer md:hidden`}>
+          <span className="transition-all duration-500 ease-in-out h-[2px] w-5 bg-black-500 rounded-full"></span>
+          <span className="transition-all duration-500 ease-in-out h-[2px] w-4 bg-black-500 rounded-full"></span>
+          <span className="transition-all duration-500 ease-in-out h-[2px] w-5 bg-black-500 rounded-full"></span>
+        </div>
+
+        {/* Menu */}
+        <div className="hidden md:flex items-center gap-8 text-black-200">
+          <p>Product</p>
+          <p>Solutions</p>
+          <p>Pricing</p>
+          <p>Resources</p>
+          <p>Customers</p>
         </div>
       </div>
     </nav>
