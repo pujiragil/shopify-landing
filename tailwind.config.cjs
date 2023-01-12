efeac4bfafb83e6d93ffd91950b01591bd17b3c0
pujiragil <ragilsg164@gmail.com>
2023-01-12 21:07:38 +0700

    config: add configuration theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: {
+        100: "#004C3F"
+      },
+      black: {
+        100: "#212625"
+      }
+    },
     extend: {},
   },
   plugins: [],
