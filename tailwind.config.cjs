91e9f1524db750974e802ba6b15a07be15d5304a
pujiragil <ragilsg164@gmail.com>
2023-01-12 23:03:21 +0700

    config: wrap customize colors to extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,26 +5,28 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      primary: {
-        100: "#004C3F",
-        200: "#9DEDC6"
+    extend: {
+      colors: {
+        primary: {
+          100: "#004C3F",
+          200: "#9DEDC6"
+        },
+        black: {
+          100: "#212625",
+          200: "#424C4A",
+          300: "#57605E",
+          400: "#82918E",
+          500: "#212326"
+        },
+        secondary: {
+          200: "#FFBC9F"
+        },
+        tertiary: {
+          200: "#FFDB95",
+          300: "#FBF7EC"
+        }
       },
-      black: {
-        100: "#212625",
-        200: "#424C4A",
-        300: "#57605E",
-        400: "#82918E"
-      },
-      secondary: {
-        200: "#FFBC9F"
-      },
-      tertiary: {
-        200: "#FFDB95",
-        300: "#FBF7EC"
-      }
     },
-    extend: {},
   },
   plugins: [],
 }
