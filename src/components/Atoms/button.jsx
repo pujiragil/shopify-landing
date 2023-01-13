eddc347178f517a7de26e6b88bdf22168403163f
pujiragil <ragilsg164@gmail.com>
2023-01-13 21:28:05 +0700

    atoms: fix button width

--- src/components/Atoms/button.jsx
+++ src/components/Atoms/button.jsx
@@ -5,7 +5,7 @@ export default function MainButton({ children, primary }) {
         primary
           ? "border border-primary-100 bg-primary-100 text-tertiary-200"
           : "border border-primary-100 bg-tertiary-300 text-primary-100"
-      } p-5 text-lg uppercase font-body font-bold`}
+      } p-5 text-lg uppercase font-body font-bold md:px-[34px] md:py-5`}
     >
       {children}
     </button>
