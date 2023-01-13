8e13328090505763ed5c44068a56d1e32cb23b88
pujiragil <ragilsg164@gmail.com>
2023-01-13 21:29:24 +0700

    root: apply main component

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
+}
