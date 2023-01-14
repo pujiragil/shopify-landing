db2f6ec0196249ee7acc349f4ff63148509b8ef6
pujiragil <ragilsg164@gmail.com>
2023-01-14 15:53:36 +0700

    comp: add responsive size hero (desktop)

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,24 +2,24 @@ import MainButton from "../Atoms/button";
 
 export default function Hero() {
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-col gap-[44px] p-5 pt-6  min-h-screen h-auto md:justify-center md:p-0 lg:min-h-fit lg:justify-start">
-        <div className="flex flex-col gap-8">
-          <section className="flex flex-col gap-5 md:items-center text-center">
-            <h1 className="text-[42px] md:text-5xl md:w-4/5 font-bold font-head text-primary-100">
+    <div className="container mx-auto max-w-[1344px] px-6 lg:py-10">
+      <div className="flex flex-col gap-[44px] p-5 pt-6  min-h-screen h-auto md:justify-center md:p-0 lg:min-h-fit lg:items-center lg:flex-row lg:justify-between">
+        <div className="flex flex-col gap-8 lg:w-full lg:gap-[34px]">
+          <section className="flex flex-col gap-5 md:items-center text-center lg:text-left lg:gap-6 lg:items-start">
+            <h1 className="text-[42px] leading-[52px] md:text-5xl md:w-4/5 font-bold font-head text-primary-100 lg:text-6xl lg:leading-[70px]">
               The platform commerce is built on.
             </h1>
-            <p className="text-lg font-body text-black-300 md:w-2/3">
+            <p className="text-lg font-body text-black-300 md:w-full lg:w-4/5">
               Millions of the world's most successful brands trust Shopify to
               sell, ship and process payments anywhere.
             </p>
           </section>
-          <div className="flex flex-col md:flex-row justify-center gap-[18px] md:gap-[30px]">
+          <div className="flex flex-col md:flex-row justify-center gap-[18px] md:gap-[30px] lg:justify-start lg:w-4/5">
             <MainButton primary={true}>admin demo</MainButton>
             <MainButton primary={false}>shop demo</MainButton>
           </div>
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center lg:w-full">
           <div className="relative w-fit">
             {/* main card */}
             <img
@@ -52,8 +52,14 @@ export default function Hero() {
                   TOTAL SALES
                 </h5>
                 <div className="w-full flex items-end justify-between">
-                  <h4 className="text-xs text-[#064A4A] font-bold md:text-2xl">$218</h4>
-                  <img className="w-8 h-auto object-cover md:w-[65px]" src="/chart.svg" alt="chart" />
+                  <h4 className="text-xs text-[#064A4A] font-bold md:text-2xl">
+                    $218
+                  </h4>
+                  <img
+                    className="w-8 h-auto object-cover md:w-[65px]"
+                    src="/chart.svg"
+                    alt="chart"
+                  />
                 </div>
               </div>
               <div className="pt-1 border-t-[0.5px] border-[#C9C9C9] w-full flex items-center justify-between text-[#818181] text-[4.38px] md:pt-2 md:text-[9px]">
