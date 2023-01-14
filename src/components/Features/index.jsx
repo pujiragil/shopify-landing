bdad3b7c1bafa1a755b946d643e7d2ed56b408da
pujiragil <ragilsg164@gmail.com>
2023-01-14 20:32:57 +0700

    comp: create features component

---
+++ src/components/Features/index.jsx
@@ -0,0 +1,9 @@
+export default function Features() {
+  return(
+    <div className="container mx-auto max-w-[1344px]">
+      <div className="flex flex-col gap-10">
+        <div className="flex"></div>
+      </div>
+    </div>
+  )
+}
