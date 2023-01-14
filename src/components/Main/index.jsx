14f8901e02eae270471160f4a564e9605fbd7ea1
pujiragil <ragilsg164@gmail.com>
2023-01-14 21:08:59 +0700

    comp: apply features component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,11 @@
+import Features from "../Features";
 import Hero from "../Hero";
 
 export default function Main() {
   return (
     <main className="bg-tertiary-300">
-      <Hero/>
+      <Hero />
+      <Features />
     </main>
-  )
+  );
 }
