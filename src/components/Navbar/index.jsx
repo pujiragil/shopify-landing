8fd7f7825f79ab4390b761e312570790bce01545
pujiragil <ragilsg164@gmail.com>
2023-01-14 21:08:21 +0700

    comp: split nav items with separate file

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import NavbarDropdown from "./navbarDropdown";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -85,47 +86,5 @@ export default function Navbar() {
         <NavbarDropdown isOpen={isOpen} />
       </div>
     </nav>
-  );
-}
-
-function NavbarDropdown({ isOpen }) {
-  return (
-    <div
-      className={`${
-        isOpen ? "translate-y-0" : "-translate-y-full"
-      } pt-[75.55px] md:hidden bg-tertiary-300 absolute top-0 left-0 w-full h-screen text-center transition-all duration-500 ease-in-out p-5 z-10`}
-    >
-      <div className="flex flex-col gap-16 mt-16">
-        <div className="flex flex-col gap-8 items-center text-black-200">
-          <a className="w-fit" href="#">
-            <p className="navlink active-navlink">Product</p>
-          </a>
-          <a className="w-fit" href="#">
-            <p className="navlink">Solutions</p>
-          </a>
-          <a className="w-fit" href="#">
-            <p className="navlink">Pricing</p>
-          </a>
-          <a className="w-fit" href="#">
-            <p className="navlink">Resources</p>
-          </a>
-          <a className="w-fit" href="#">
-            <p className="navlink">Customers</p>
-          </a>
-        </div>
-        <div className="flex flex-col gap-8">
-          <a href="#">
-            <p className="underline text-black-100 py-3.5 border border-black-100">
-              Login
-            </p>
-          </a>
-          <a href="#">
-            <button className="py-3.5 border border-primary-100 w-full text-white bg-primary-100 font-medium">
-              Start Free
-            </button>
-          </a>
-        </div>
-      </div>
-    </div>
   );
 }
