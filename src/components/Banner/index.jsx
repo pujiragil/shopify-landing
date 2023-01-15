7a4cc596c4a2a8c0977966aeb969736357abc003
pujiragil <ragilsg164@gmail.com>
2023-01-15 08:27:17 +0700

    comp: create new banner component

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,9 @@
+export default function Banner() {
+  return (
+    <div className="bg-primary-100">
+      <div className="container mx-auto max-w-[1344px]">
+        
+      </div>
+    </div>
+  )
+}
