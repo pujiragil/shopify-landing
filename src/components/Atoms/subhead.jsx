4a738bba328abf3d9fc735d825fc14fc8f01948f
pujiragil <ragilsg164@gmail.com>
2023-01-15 07:42:33 +0700

    atoms: add different font size

--- src/components/Atoms/subhead.jsx
+++ src/components/Atoms/subhead.jsx
@@ -1,3 +1,3 @@
 export default function SubHead({ children, width, style }) {
-  return <h2 className={`${width ? width : "w-full"} font-head text-[2rem] font-medium text-primary-100 ${style}`}>{children}</h2>
+  return <h2 className={`${width ? width : "w-full"} font-head text-[2rem] md:text-[2.5rem] lg:text-[50px] font-medium text-primary-100 ${style}`}>{children}</h2>
 }
