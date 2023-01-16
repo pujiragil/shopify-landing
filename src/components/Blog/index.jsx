c2c3140bfba4b7494d8432569b95d6b2f2e14557
pujiragil <ragilsg164@gmail.com>
2023-01-16 13:41:36 +0700

    comp: create responsive blog(sm, md, lg)

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -5,7 +5,7 @@ import { Button } from "../Atoms/button";
 export default function Blog() {
   return (
     <div className="container mx-auto max-w-[1344px]">
-      <div className="px-5 py-20 flex flex-col gap-[50px]">
+      <div className="px-5 py-20 flex flex-col gap-[50px] sm:px-10">
         {/* Head Content */}
         <div className="flex flex-col gap-8">
           <div className="flex flex-col gap-[18px]">
@@ -21,20 +21,20 @@ export default function Blog() {
         </div>
 
         {/* Blog Items */}
-        <div className="grid gap-8">
+        <div className="grid gap-8 font-body md:grid-cols-2 lg:gap-12">
           <section className="flex flex-col gap-6">
             <div className="overflow-hidden">
               <img className="w-full h-auto object-cover hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer" src="/blog-1.png" alt="thumbnail" />
             </div>
-            <h3 className="text-2xl font-medium font-body text-black-100">Improve your business ecommerce management</h3>
-            <a className="underline font-body text-black-100" href="#">Read More</a>
+            <h3 className="text-[26px] font-medium text-black-100">Improve your business ecommerce management</h3>
+            <a className="underline text-black-100" href="#">Read More</a>
           </section>
           <section className="flex flex-col gap-6">
             <div className="overflow-hidden">
               <img className="w-full h-auto object-cover hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer" src="/blog-2.png" alt="thumbnail" />
             </div>
-            <h3 className="text-2xl font-medium font-body text-black-100">Ecommerce Hosting: What to Look for in a Host</h3>
-            <a className="underline font-body text-black-100" href="#">Read More</a>
+            <h3 className="text-[26px] font-medium text-black-100">Ecommerce Hosting: What to Look for in a Host</h3>
+            <a className="underline text-black-100" href="#">Read More</a>
           </section>
         </div>
       </div>
