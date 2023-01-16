89363cf1d74d027ba66fc35a01d6595985c6227c
pujiragil <ragilsg164@gmail.com>
2023-01-16 12:42:10 +0700

    comp: create blog component

---
+++ src/components/Blog/index.jsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+  return (
+    <div className="container mx-auto max-w-[1344px]">
+      <div className="px-5 py-20 flex flex-col gap-[50px]">
+        
+      </div>
+    </div>
+  );
+}
