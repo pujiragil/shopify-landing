785ced5cbde3bc4ccd2ab46962d0fe6cb2886012
pujiragil <ragilsg164@gmail.com>
2023-01-16 10:32:35 +0700

    root: apply testimonial component

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import Banner from "./components/Banner";
 import Card from "./components/Card";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
+import Testimonial from "./components/Testimonial";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Main />
       <Banner />
       <Card />
+      <Testimonial />
     </>
   );
 }
