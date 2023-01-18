b913a315122bf054b3ba141cd80c20455996e7a5
pujiragil <ragilsg164@gmail.com>
2023-01-18 21:36:16 +0700

    comp: add animation card component

--- src/components/Card/cardItem.jsx
+++ src/components/Card/cardItem.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Button } from "../Atoms/button";
 import Paragraph from "../Atoms/paragraph";
 import SubHead from "../Atoms/subhead";
@@ -10,16 +11,46 @@ export default function CardItem({
   img,
   primary,
 }) {
+  const cardContent = {
+    visible: { x: 0, opacity: 1, transition: { duration: 1 } },
+    hidden: { x: 100, opacity: 0 },
+  };
+
+  const cardImage = {
+    visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+    hidden: { opacity: 0, scale: 0.5 },
+  };
+
   return (
     <div className="flex flex-col gap-[50px] md:flex-row md:gap-5 sm:items-center">
-      <section className={`${primary ? "md:order-2" : "md:order-1"} w-full`}>
+      <motion.section
+        initial="hidden"
+        whileInView="visible"
+        variants={cardContent}
+        className={`${primary ? "md:order-2" : "md:order-1"} w-full`}
+      >
         <Subtitle style="mb-2.5">{subtitle}</Subtitle>
-        <SubHead style="mb-[18px] sm:w-4/5 md:w-full lg:w-11/12">{subhead}</SubHead>
-        <Paragraph style="mb-[30px] w-4/5 sm:w-8/12 md:w-4/5 lg:w-8/12">{paragraph}</Paragraph>
+        <SubHead style="mb-[18px] sm:w-4/5 md:w-full lg:w-11/12">
+          {subhead}
+        </SubHead>
+        <Paragraph style="mb-[30px] w-4/5 sm:w-8/12 md:w-4/5 lg:w-8/12">
+          {paragraph}
+        </Paragraph>
         <Button>Buy Now</Button>
-      </section>
-      <div className={`${primary ? "md:order-1" : "md:order-2"} sm:w-2/3 md:w-full lg:w-4/5`}>
-        <img className="w-full h-auto object-cover" src={img} alt="card" />
+      </motion.section>
+      <div
+        className={`${
+          primary ? "md:order-1" : "md:order-2"
+        } sm:w-2/3 md:w-full lg:w-4/5`}
+      >
+        <motion.img
+          initial="hidden"
+          whileInView="visible"
+          variants={cardImage}
+          className="w-full h-auto object-cover"
+          src={img}
+          alt="card"
+        />
       </div>
     </div>
   );
