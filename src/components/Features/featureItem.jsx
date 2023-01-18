6271c9988395c203f84beeeaea75a2e722bb330c
pujiragil <ragilsg164@gmail.com>
2023-01-18 11:58:50 +0700

    comp: add feature item animation

--- src/components/Features/featureItem.jsx
+++ src/components/Features/featureItem.jsx
@@ -1,8 +1,24 @@
 import Paragraph from "../Atoms/paragraph";
+import { motion } from "framer-motion";
 
 export default function FeatureItem({ feature }) {
+  const featureItem = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: .7, },
+    },
+    hidden: { opacity: 0, x: 100 },
+  };
+
   return (
-    <section className="flex flex-col border border-black-200 feature-item">
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      variants={featureItem}
+      viewport={{ amount: 0.01 }}
+      className="flex flex-col border border-black-200 feature-item"
+    >
       <div className="px-5 py-2.5 flex items-center justify-between border-b border-black-200">
         <h4 className="text-black-100 font-body font-medium text-lg">
           {feature?.title}
@@ -12,6 +28,6 @@ export default function FeatureItem({ feature }) {
       <div className="p-6 pb-10">
         <Paragraph>{feature?.content}</Paragraph>
       </div>
-    </section>
+    </motion.section>
   );
 }
