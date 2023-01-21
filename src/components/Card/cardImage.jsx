b498f346443bbc73c1f5f64e312ff5c04120f72a
pujiragil <ragilsg164@gmail.com>
2023-01-21 10:26:19 +0700

    comp: add style (md) size

--- src/components/Card/cardImage.jsx
+++ src/components/Card/cardImage.jsx
@@ -1,7 +1,7 @@
 export default function CardImage() {
   return (
-    <div className="w-11/12 max-w-[380px] sm:max-w-[480px] lg:w-11/12 p-5 relative z-10 after:bg-tertiary-200 after:w-full after:h-[85%] after:absolute after:inset-0 after:z-0 sm:p-8 md:p-10">
-      <div className="relative z-10 bg-white px-3.5 py-[18px] sm:px-5 sm: py-7 md:px-6 md:py-9 flex flex-col gap-[18px] shadow shadow-[rgba(182, 161, 110, 0.19)] font-head lg:gap-[34px] lg:shadow-lg">
+    <div className="w-11/12 max-w-[380px] sm:max-w-[480px] md:w-full md:max-w-none lg:w-11/12 p-5 relative z-10 after:bg-tertiary-200 after:w-full after:h-[85%] after:absolute after:inset-0 after:z-0 sm:p-8 md:p-4 lg:p-10">
+      <div className="relative z-10 bg-white px-3.5 py-[18px] sm:px-5 sm: py-7 md:px-6 md:py-9 flex flex-col gap-[18px] shadow shadow-[rgba(182, 161, 110, 0.19)] font-head lg:gap-[34px] md:shadow-md lg:shadow-lg">
         <div className="flex items-center justify-between pb-4 md:pb-8 border-b-[0.5px] md:border-b border-[#9C9C9C]">
           <h4 className="text-xs text-primary-100 font-medium sm:text-base lg:text-2xl">
             Shopify Bill Payment
@@ -72,7 +72,7 @@ export default function CardImage() {
       </div>
 
       {/* mini modal */}
-      <div className="absolute w-2/5 z-10 -bottom-8 -right-2 px-3.5 py-4 bg-white shadow shadow-[rgba(150, 126, 67, 0.08)] rounded-lg flex flex-col gap-5 font-head text-center sm:gap-8 sm:-right-4 lg:px-6 lg:py-8 lg:w-1/2 lg:gap-10 lg:shadow-lg lg:-bottom-14 lg:-right-8">
+      <div className="absolute w-2/5 z-10 -bottom-8 -right-2 px-3.5 py-4 bg-white shadow shadow-[rgba(150, 126, 67, 0.08)] rounded-lg flex flex-col gap-5 font-head text-center sm:gap-8 sm:-right-4 md:w-3/5 ms:shadow-md md:max-w-[200px] lg:px-6 lg:py-8 lg:w-1/2 lg:gap-10 lg:shadow-lg lg:-bottom-14 lg:-right-8">
         <div>
           <h5 className="text-[10px] font-medium text-primary-100 mb-1.5 sm:text-base lg:text-lg lg:mb-2">Bill Amount</h5>
           <p className="text-black-300 text-[8px] sm:text-xs lg:text-base">A fair way to divide bills</p>
