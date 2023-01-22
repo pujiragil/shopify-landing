bd148831b68715824e43e8e9cb61f8cf6e596b89
pujiragil <ragilsg164@gmail.com>
2023-01-22 13:54:00 +0700

    comp: split card image

--- src/components/Card/cardImage.jsx
+++ src/components/Card/cardImage.jsx
@@ -1,102 +1,61 @@
 import { motion } from "framer-motion";
 import CardProfileList from "./cardProfileList";
 
-export default function CardImage({ primary }) {
-  const primaryContainer = {
-    visible: {
-      y: 0,
-      transition: { duration: 0.5, delayChildren: 0.5, staggerChildren: 0.1 },
-    },
-    hidden: { y: 100 },
-  };
-
-  const miniModal = {
-    visible: {
-      y: 0,
-      transition: { duration: 0.5, delay: 0.5 },
-    },
-    hidden: {
-      y: 100,
-    },
-  };
-
-  const fakeProfiles = [
-    {
-      id: 0,
-      avatar: "/profile-1.png",
-      name: "Jenny Wilson",
-      phone: "(629) 555-0129",
-      amount: "$739.65",
-    },
-    {
-      id: 1,
-      avatar: "/profile-2.png",
-      name: "Terry Robar",
-      phone: "(612) 675-8829",
-      amount: "$475.22",
-    },
-    {
-      id: 2,
-      avatar: "/profile-3.png",
-      name: "Pritom Rana",
-      phone: "(129) 325-0021",
-      amount: "$839.65",
-    },
-  ];
-
-  if (primary) {
-    return (
-      <div className="w-11/12 max-w-[380px] sm:max-w-[480px] md:w-full md:max-w-none lg:w-11/12 p-5 relative z-10 after:bg-tertiary-200 after:w-full after:h-[85%] after:absolute after:inset-0 after:z-0 sm:p-8 md:p-4 lg:p-10">
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          variants={primaryContainer}
-          className="relative z-10 bg-white px-3.5 py-[18px] sm:px-5 sm: py-7 md:px-6 md:py-9 flex flex-col gap-[18px] font-head lg:gap-[34px] primary-card"
-        >
-          <div className="flex items-center justify-between pb-4 md:pb-8 border-b-[0.5px] md:border-b border-[#9C9C9C]">
-            <h4 className="text-xs text-primary-100 font-medium sm:text-base lg:text-2xl">
-              Shopify Bill Payment
-            </h4>
-            <span className="text-primary-100 text-[7px] font-medium bg-[#EBF7F7] w-4 h-4 flex items-center justify-center block rounded-full sm:w-5 sm:h-5 sm:text-[10px] lg:w-8 lg:h-8 lg:text-base">
-              3
-            </span>
-          </div>
-          <div className="grid gap-3.5 overflow-hidden">
-            {fakeProfiles.map((profile) => (
-              <CardProfileList key={profile.id} profile={profile} />
-            ))}
-          </div>
-          <button className="py-2.5 bg-primary-100/10 w-full text-primary-100 font-body font-medium text-[8px] sm:py-4 sm:text-xs lg:py-[22px] lg:text-base">
-            Add Manual
-          </button>
-        </motion.div>
-
-        {/* mini modal */}
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          variants={miniModal}
-          className="absolute w-2/5 z-10 -bottom-8 -right-2 px-3.5 py-4 bg-white rounded-lg flex flex-col gap-5 font-head text-center sm:gap-8 sm:-right-4 md:w-3/5 md:max-w-[200px] lg:px-6 lg:py-8 lg:w-1/2 lg:gap-10 lg:-bottom-10 lg:-right-8 lg:max-w-none mini-modal"
-        >
-          <div>
-            <h5 className="text-[10px] font-medium text-primary-100 mb-1.5 sm:text-base lg:text-lg lg:mb-2">
-              Bill Amount
-            </h5>
-            <p className="text-black-300 text-[8px] sm:text-xs lg:text-base">
-              A fair way to divide bills
-            </p>
-          </div>
-          <h4 className="font-medium text-[18px] text-primary-100 sm:text-2xl lg:text-4xl">
-            $739.65
+export function PrimaryCard({ primaryContainer, miniModal, fakeProfiles }) {
+  return (
+    <div className="w-11/12 max-w-[380px] sm:max-w-[480px] md:w-full md:max-w-none lg:w-11/12 p-5 relative z-10 after:bg-tertiary-200 after:w-full after:h-[85%] after:absolute after:inset-0 after:z-0 sm:p-8 md:p-4 lg:p-10">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={primaryContainer}
+        className="relative z-10 bg-white px-3.5 py-[18px] sm:px-5 sm: py-7 md:px-6 md:py-9 flex flex-col gap-[18px] font-head lg:gap-[34px] primary-card"
+      >
+        <div className="flex items-center justify-between pb-4 md:pb-8 border-b-[0.5px] md:border-b border-[#9C9C9C]">
+          <h4 className="text-xs text-primary-100 font-medium sm:text-base lg:text-2xl">
+            Shopify Bill Payment
           </h4>
-          <button className="bg-[#FFBC9F] text-[8px] text-primary-100 py-2 w-full sm:text-xs sm:py-2.5 lg:text-base lg:py-4">
-            Quick Pay
-          </button>
-        </motion.div>
-      </div>
-    );
-  }
+          <span className="text-primary-100 text-[7px] font-medium bg-[#EBF7F7] w-4 h-4 flex items-center justify-center block rounded-full sm:w-5 sm:h-5 sm:text-[10px] lg:w-8 lg:h-8 lg:text-base">
+            3
+          </span>
+        </div>
+        <div className="grid gap-3.5 overflow-hidden">
+          {fakeProfiles.map((profile) => (
+            <CardProfileList key={profile.id} profile={profile} />
+          ))}
+        </div>
+        <button className="py-2.5 bg-primary-100/10 w-full text-primary-100 font-body font-medium text-[8px] sm:py-4 sm:text-xs lg:py-[22px] lg:text-base">
+          Add Manual
+        </button>
+      </motion.div>
 
+      {/* mini modal */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        variants={miniModal}
+        className="absolute w-2/5 z-10 -bottom-8 -right-2 px-3.5 py-4 bg-white rounded-lg flex flex-col gap-5 font-head text-center sm:gap-8 sm:-right-4 md:w-3/5 md:max-w-[200px] lg:px-6 lg:py-8 lg:w-1/2 lg:gap-10 lg:-bottom-10 lg:-right-8 lg:max-w-none mini-modal"
+      >
+        <div>
+          <h5 className="text-[10px] font-medium text-primary-100 mb-1.5 sm:text-base lg:text-lg lg:mb-2">
+            Bill Amount
+          </h5>
+          <p className="text-black-300 text-[8px] sm:text-xs lg:text-base">
+            A fair way to divide bills
+          </p>
+        </div>
+        <h4 className="font-medium text-[18px] text-primary-100 sm:text-2xl lg:text-4xl">
+          $739.65
+        </h4>
+        <button className="bg-[#FFBC9F] text-[8px] text-primary-100 py-2 w-full sm:text-xs sm:py-2.5 lg:text-base lg:py-4">
+          Quick Pay
+        </button>
+      </motion.div>
+    </div>
+  );
+}
+
+export function SecondaryCard() {
   return (
     <div className="w-11/12 max-w-[380px] sm:max-w-[480px] md:w-full md:max-w-none lg:w-11/12 bg-secondary-200 p-5 flex flex-col gap-2.5 font-body text-primary-100 font-medium sm:p-8">
       <div className="bg-white py-2 px-2.5 flex justify-between items-center rounded-md shadow sm:py-3.5 sm:px-4 md:shadow-md lg:py-4 lg:px-5 lg:rounded-xl">
@@ -136,7 +95,7 @@ export default function CardImage({ primary }) {
         </div>
       </div>
       <div className="bg-white p-4 pb-5 flex flex-col gap-7 rounded-md shadow sm:p-6 sm:pb-7 md:shadow-md lg:p-7 lg:pb-9 lg:rounded-xl">
-        <div className="flex justify-between items-center">
+        <div className="flex gap-6 items-center">
           <p className="text-[9px] sm:text-sm lg:text-base">Total Growth</p>
           <img
             className="w-2.5 h-auto object-cover sm:w-4 lg:w-5"
